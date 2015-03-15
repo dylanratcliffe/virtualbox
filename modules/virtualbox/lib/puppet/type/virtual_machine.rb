de3fd0a19134cbef352960228f8c47e5a478350e
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2015-03-15 21:56:52 +1100

    Commented out groups as they are confusing and people probabaly wont want them, might want to put it back in later when i have time

--- modules/virtualbox/lib/puppet/type/virtual_machine.rb
+++ modules/virtualbox/lib/puppet/type/virtual_machine.rb
@@ -13,15 +13,15 @@ Puppet::Type.newtype(:virtual_machine) do
   	
   end
 
-  newproperty(:groups, :array_matching => :all) do # :array_matching defaults to :first
-    desc "Groups that the VM will belong to. (Optional)"
-    # This little gem makes sure that when we are comparing the arrays
-    # to see if they match up, we sort them first so that puppet doesn't
-    # freak the fuck out
-    def insync?(is)
-      is.sort == should.sort
-    end
-  end
+  #newproperty(:groups, :array_matching => :all) do # :array_matching defaults to :first
+  #  desc "Groups that the VM will belong to. (Optional)"
+  #  # This little gem makes sure that when we are comparing the arrays
+  #  # to see if they match up, we sort them first so that puppet doesn't
+  #  # freak the fuck out
+  #  def insync?(is)
+  #    is.sort == should.sort
+  #  end
+  #end
 
   newproperty(:ostype) do
   	desc "OS type of the VM (Optional)"
