f44a9bddb54303a8fe6b7e99b4eff8f6b65a2880
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2015-03-15 21:36:52 +1100

    Added exists?

--- modules/virtualbox/lib/puppet/provider/virtualbox/virtualbox_vm.rb
+++ modules/virtualbox/lib/puppet/provider/virtualbox/virtualbox_vm.rb
@@ -7,6 +7,11 @@ Puppet::Type.type(:virtual_machine).provide(:virtualbox_vm, :parent => Puppet::P
     commands :vboxmanage => 'vboxmanage'
 
     def exists?
+      # The logic here is that calling this will throw an exception if it
+      # does not exist and if it does the resulting string or array or 
+      # whatever will be cast into a true. Would be good if there was a 
+      # way to test it, maybe I could put in a bunch of debugging??
+      vboxmanage(['showvminfo', resource[:name]])
     end
 
     def create
@@ -27,14 +32,14 @@ Puppet::Type.type(:virtual_machine).provide(:virtualbox_vm, :parent => Puppet::P
       uuid_flag = '--uuid' if resource[:uuid]
 
       begin
-      # Execute vboxmanage to create the vm
-      output = vboxmanage(['createvm',
-      	name_flag, resource[:name],
-      	groups_flag, resource[:groups],
-      	ostype_flag, resource[:ostype],
-      	register_flag,
-      	basefolder_flag, resource[:basefolder],
-      	uuid_flag, resource[:uuid]])      
+        # Execute vboxmanage to create the vm
+        output = vboxmanage(['createvm',
+          name_flag, resource[:name],
+          groups_flag, resource[:groups],
+          ostype_flag, resource[:ostype],
+          register_flag,
+          basefolder_flag, resource[:basefolder],
+          uuid_flag, resource[:uuid]])      
       rescue Puppet::ExecutionFailure => e
         Puppet.debug("#vboxmanage had an error -> #{e.inspect}")
         throw e
