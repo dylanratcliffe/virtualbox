9b6fcb789ab5a8b64ed95727ef8c76299d81fe69
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2015-03-15 18:03:09 +1100

    Added a bunch of extra params and properties to the type definition

--- modules/virtualbox/lib/puppet/type/vbox_vm.rb
+++ modules/virtualbox/lib/puppet/type/vbox_vm.rb
@@ -6,6 +6,36 @@ Puppet::Type.newtype(:vbox_vm) do
   # on this resource
   ensurable
 
-  
   # ... the code ...
+  newparam(:name) do
+  	desc "The name of the VM, must not contain spaces"
+  	validate do |value|
+  	  unless value =~ /[.\S]+/
+  	  	raise ArgumentError, "%s is not a valid name" % value
+  	  end
+  end
+
+  newproperty(:groups, :array_matching => :all) do # :array_matching defaults to :first
+    desc "Groups that the VM will belong to. (Optional)"
+  end
+
+  newproperty(:ostype) do
+  	desc "OS type of the VM (Optional)"
+  	# TODO: Find out what the valid values are and do some validation
+  end
+
+  newproperty(:register) do
+  	desc "Weather or not to register the new Vm with VirtualBox (Optional)"
+    # TODO: Make this default to true
+    newvalue(:true)
+    newvalue(:false)
+  end
+
+  newproperty(:basefolder) do
+  	desc "Location for the VM (Optional)"
+  end
+
+  newproperty(:uuid) do
+  	desc "UUID of the new VM (Optional)"
+  end
 end
